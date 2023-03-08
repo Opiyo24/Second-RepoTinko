bfa893f4f1b21e358da44e3622058c45377d4c08
Opiyo24 <gabbynyangor@gmail.com>
2023-03-08 14:53:59 +0300

    3 Versions of Hello World

--- Dir6/Tinkolilo.c
+++ Dir6/Tinkolilo.c
@@ -1,2 +1,34 @@
 This is the first C file in this directory.
 From here, I will go to Visual Studio Code to write the three variations of Hello World we learned about.
+
+/* First C program: Hello World*/
+/* #include <stdio.h> */
+
+int main(void)
+{
+    printf("Hello World!\n");
+}
+
+/* Hello World version 2*/
+/* #include <stdio.h> */
+
+int main(void)
+{
+    printf("Hello ")
+    printf("World!")
+    printf("\n")
+}
+
+/* Hello World version 3 */
+
+int main(void)
+{
+    int i = 0
+    char *str = "Hello World!\n"
+
+    /* Print each character until reach '\0' */
+    while {str[i]! = '\0'}
+        printf("%c", str[i++]);
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 This is my first README file for the Second-RepoTinko directory.
 All this is for my Git practise.
 Trying to master the concepts and navigation.
+
