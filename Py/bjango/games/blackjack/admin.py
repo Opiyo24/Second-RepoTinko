c5c52100c09b71d1890ecb574c11ba3783734938
Opiyo24 <gabbynyangor@gmail.com>
2024-04-27 13:43:09 +0300

    aborting

--- Py/bjango/games/blackjack/admin.py
+++ Py/bjango/games/blackjack/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+# from .models import Player, Play
 
 # Register your models here.
+# admin.site.register(Player)
+# admin.site.register(Play)
--- Py/bjango/games/blackjack/models.py
+++ Py/bjango/games/blackjack/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+#player
+# class Player(models.Model):
+#     name = models.CharField(max_length=50)
+#     score = models.IntegerField()
+
+
+#     def __str__(self):
+#         return f"{self.name} has {self.score} points."
+#first play
+# class Play(models.Model):
+#     player = models.CharField(max_length = 200)
+#     score = models.IntegerField()
+#     response1 = models.BooleanField(default=False)
+#     score2 = models.IntegerField()
+#     response2 = models.BooleanField(default=True)
+#     score3 = models.IntegerField()
+#     response3 = models.BooleanField(default=True)
+#     score4 = models.IntegerField()
+
