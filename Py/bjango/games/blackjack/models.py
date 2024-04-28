5edae1806eb286bf6033b2fb21f4b7ddb074482a
Opiyo24 <gabbynyangor@gmail.com>
2024-04-28 17:21:09 +0300

    Precautionary commit

--- Py/bjango/games/blackjack/models.py
+++ Py/bjango/games/blackjack/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+    name = models.CharField(max_length = 200)
+    capital = models.IntegerField(default=1000)
+
+
+    def __str__(self):
+        return self.name
+    
+
+class Play(models.Model):
+    player = models.CharField(max_length = 200)
+    score = models.IntegerField()
+    response = models.BooleanField(default=False)
+    score2 = models.IntegerField()
+    response2 = models.BooleanField(default=False)
+    score3 = models.IntegerField()
+    response2 = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.player
--- Py/bjango/games/blackjack/views.py
+++ Py/bjango/games/blackjack/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Player
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Welcome to Blackjack!")
+    form = PlayerForm()
+    context = {
+        "form": form
+    }
+    return render(request, 'blackjack/index.html', context)
+
+class PlayerForm(forms.ModelForm):
+    class Meta:
+        model = Player
+        fields = "__all__"
+
+
+def create_player(request):
+    if request.method == 'POST':
+        form = PlayerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
