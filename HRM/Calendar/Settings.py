e0588edb5a071f9374bd16aeda8200349120f604
Opiyo24 <gabbynyangor@gmail.com>
2024-05-15 07:09:49 +0300

    Calendar-like events

---
+++ HRM/Calendar/Settings.py
@@ -0,0 +1,21 @@
+//devproject/settings.py
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'myapp', #add myapp
+]
+        
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
+    }
+}
